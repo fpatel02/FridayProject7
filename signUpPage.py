78460c236705065aabb61c677db58fc66b6120dd
fpatel02 <patelfauram1@outlook.com>
2024-03-31 23:39:22 -0500

    Wrote general sign up page

--- signUpPage.py
+++ signUpPage.py
@@ -1,16 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-Label(root, text = "Welcome to the Log-in page!").pack()
+Label(root, text = "Welcome to the sign-up page!").pack()
 
 emailLabel = ttk.Label(root, text = "Please enter your email address here").pack()
 emailEntry = ttk.Entry(root, text = "Email address: ").pack()
 
 passwordLabel = ttk.Label(root, text = "Please enter your password here").pack()
 passwordEntry = ttk.Entry(root, text = "Password: ", show = '*').pack()
-
-confirmPasswordLabel = ttk.Label(root, text = "Confirm your password to sign in").pack()
-confirmPasswordEntry = ttk.Entry(root, text = "Confirm password: ", show = '*').pack()
 
 submitButton = ttk.Button(root, text = "Log in").pack()
 
